0fa9f7b4fb83f5f421f3c70d4f4532602c2df243
Rafael Ortiz <rortiz@inkremental.co>
2023-05-27 19:02:53 -0500

    Role model and validation from DB

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
+const Role = require('../models/role');
+const { validateFields } = require('../middlewares/validate-fields');
 const { getUsers, 
         createUser, 
         updatePutUser, 
         updatePatchUser, 
         deleteUser } = require('../controllers/users');
-const { check } = require('express-validator');
-const { validateFields } = require('../middlewares/validate-fields');
 
 const router = Router();
 
@@ -17,7 +18,12 @@ router.post('/', [
         check('email', 'Email is not valid').isEmail(),
         check('password', 'Password must be at least 6 characters').isLength({ min: 6 }).not(),
         check('password', 'Password is required').not().isEmpty(),
-        check('role', 'Role is not valid').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+        check('role').custom(async(role = '') => {
+                const existsRole = await Role.findOne({ role });
+                if (!existsRole) {
+                        throw new Error(`Role ${role} is not registered in the database`);
+                }
+        }),
         validateFields
 ], createUser);
 
